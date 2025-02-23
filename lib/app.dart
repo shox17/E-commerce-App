a066f2fc1c40788f328f7a3920ca6b7ac12ed7d6
shox17 <159898318+shox17@users.noreply.github.com>
2025-02-23 18:38:32 +0900

    OnBoarding complete

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:ecommerce_app/utils/constants/text_strings.dart';
@@ -15,14 +16,7 @@ class App extends StatelessWidget {
       darkTheme: TAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       // initialBinding: GeneralBindings(),
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'Awesome! 🎊 Project Structure is set up and running. \n Happy T Coding 🎊',
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
+      home: const OnboardingScreen(),
     );
   }
 }
