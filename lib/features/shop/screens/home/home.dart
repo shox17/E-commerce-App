5e82858a1f788f26d60cd82315c5460244c403d0
shox17 <159898318+shox17@users.noreply.github.com>
2025-03-07 12:52:31 +0900

    Add Search Container

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,10 @@
 import 'package:ecommerce_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:ecommerce_app/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:ecommerce_app/utils/constants/colors.dart';
+import 'package:ecommerce_app/utils/constants/sizes.dart';
+import 'package:ecommerce_app/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,7 +15,46 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TPrimaryHeaderContainer(child: Column(children: [THomeAppBar()])),
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  /// -- AppBar
+                  THomeAppBar(),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  /// -- SearchBar
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: TSizes.defaultSpace,
+                    ),
+                    child: Container(
+                      width: TDeviceUtils.getScreenWidth(context),
+                      padding: EdgeInsets.all(TSizes.md),
+                      decoration: BoxDecoration(
+                        color: TColors.white,
+                        borderRadius: BorderRadius.circular(
+                          TSizes.cardRadiusLg,
+                        ),
+                        border: Border.all(color: TColors.grey),
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Iconsax.search_normal,
+                            color: TColors.darkerGrey,
+                          ),
+                          const SizedBox(width: TSizes.spaceBtwItems),
+                          Text(
+                            'Search in Store',
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
