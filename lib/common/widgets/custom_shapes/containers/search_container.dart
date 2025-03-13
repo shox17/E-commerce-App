0dd2d468fe2ac151cfa02c9b051932eee4c1233d
shox17 <159898318+shox17@users.noreply.github.com>
2025-03-13 19:45:13 +0900

    Add Home Categories

---
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -0,0 +1,50 @@
+import 'package:ecommerce_app/utils/constants/colors.dart';
+import 'package:ecommerce_app/utils/constants/sizes.dart';
+import 'package:ecommerce_app/utils/device/device_utility.dart';
+import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TSearchContainer extends StatelessWidget {
+  const TSearchContainer({
+    super.key,
+    required this.text,
+    this.icon = Iconsax.search_normal,
+    this.showBackground = true,
+    this.showBorder = true,
+  });
+
+  final String text;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+      child: Container(
+        width: TDeviceUtils.getScreenWidth(context),
+        padding: EdgeInsets.all(TSizes.md),
+        decoration: BoxDecoration(
+          color:
+              showBackground
+                  ? dark
+                      ? TColors.dark
+                      : TColors.light
+                  : Colors.transparent,
+          borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+          border: showBorder ? Border.all(color: TColors.grey) : null,
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: TColors.darkerGrey),
+            const SizedBox(width: TSizes.spaceBtwItems),
+            Text(text, style: Theme.of(context).textTheme.bodySmall),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,13 @@
 import 'package:ecommerce_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:ecommerce_app/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:ecommerce_app/common/widgets/texts/heading_section.dart';
 import 'package:ecommerce_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:ecommerce_app/utils/constants/colors.dart';
+import 'package:ecommerce_app/utils/constants/image_strings.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
-import 'package:ecommerce_app/utils/device/device_utility.dart';
+import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -23,36 +26,107 @@ class HomeScreen extends StatelessWidget {
                   const SizedBox(height: TSizes.spaceBtwSections),
 
                   /// -- SearchBar
+                  TSearchContainer(text: 'Search in Store'),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  /// --Categories
                   Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: TSizes.defaultSpace,
-                    ),
-                    child: Container(
-                      width: TDeviceUtils.getScreenWidth(context),
-                      padding: EdgeInsets.all(TSizes.md),
-                      decoration: BoxDecoration(
-                        color: TColors.white,
-                        borderRadius: BorderRadius.circular(
-                          TSizes.cardRadiusLg,
+                    padding: const EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        /// --Heading
+                        TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                          textColor: Colors.white,
                         ),
-                        border: Border.all(color: TColors.grey),
-                      ),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Iconsax.search_normal,
-                            color: TColors.darkerGrey,
+                        const SizedBox(height: TSizes.spaceBtwItems),
+
+                        /// --Categories
+                        SizedBox(
+                          height: 80,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (_, index) {
+                              return TVerticalImageText(
+                                image: TImages.sportIcon,
+                                title: 'Sports',
+                                onTap: () {},
+                              );
+                            },
                           ),
-                          const SizedBox(width: TSizes.spaceBtwItems),
-                          Text(
-                            'Search in Store',
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TVerticalImageText extends StatelessWidget {
+  const TVerticalImageText({
+    super.key,
+    required this.image,
+    required this.title,
+    this.textColor = TColors.white,
+    this.backgroundColor = TColors.white,
+    required this.onTap,
+  });
+
+  final String image, title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(right: TSizes.spaceBtwItems),
+        child: Column(
+          children: [
+            /// Circular Icon
+            Container(
+              width: 56,
+              height: 56,
+              padding: const EdgeInsets.all(TSizes.sm),
+              decoration: BoxDecoration(
+                color:
+                    backgroundColor ??
+                    (THelperFunctions.isDarkMode(context)
+                        ? TColors.dark
+                        : TColors.white),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(
+                child: Image(
+                  image: AssetImage(image),
+                  fit: BoxFit.cover,
+                  color: TColors.dark,
+                ),
+              ),
+            ),
+
+            ///Text
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            SizedBox(
+              width: 55,
+              child: Text(
+                title,
+                style: Theme.of(
+                  context,
+                ).textTheme.labelMedium!.apply(color: textColor),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
             ),
           ],
